18a2a81d5cded030a05a7c028960c9f77647aba2
MiguelCarlosRojas <isakiangel6@gmail.com>
2025-03-09 20:00:46 -0500

    Update cors.js

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,10 +1,11 @@
 const cors = require("cors");
+require("dotenv").config();
 
 module.exports = cors({
   origin: (origin, callback) => {
     const allowedOrigins = [
-      "http://localhost:5173",
-      "https://arcami.vercel.app",
+      process.env.ALLOWED_ORIGIN_LOCAL,
+      process.env.ALLOWED_ORIGIN_PROD,
     ];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
